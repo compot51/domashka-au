2f45bd497e8da4b3ca5c04e4dee9654e5f5809b6
Yakov <72313050+compot51@users.noreply.github.com>
2022-10-14 00:26:30 +0300

    Update au_2_taylor_atan.py

--- au_2_taylor_atan.py
+++ au_2_taylor_atan.py
@@ -1,6 +1,8 @@
 import math
+import numpy
+import matplotlib.pyplot as mpp
 
-const = 100
+const = 10
 
 def my_atan(x):
     """
@@ -20,3 +22,9 @@ help(my_atan)
 
 print(math.atan(0.2))
 print(my_atan(0.2))
+
+arg = numpy.arange(-1.5, 1.5, 0.01)
+
+mpp.plot(arg, [my_atan(a) for a in arg], linewidth = 3.0, color = "red")
+mpp.plot(arg, [math.atan(a) for a in arg], linewidth = 1.0, color = "cyan")
+mpp.show()
