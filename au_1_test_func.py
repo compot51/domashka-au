29767bd85da7799cb7b3fcd90f240461ad919f61
Yakov <72313050+compot51@users.noreply.github.com>
2022-10-13 23:32:53 +0300

    Update au_1_test_func.py

--- au_1_test_func.py
+++ au_1_test_func.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as mpp  # импортируем matplotlib.pyplot ка
 
 # Эта программа рисует график функции, заданной выражением ниже
 
-if __name__ == '__main__':
-    arguments = numpy.arange(0, 200, 0.1)
+if __name__ == '__main__':  # проверка, запускается ли программа как основная 
+    arguments = numpy.arange(0, 200, 0.1)  # передаем в arguments значения от 0 до 200 с шагом 0.1
     mpp.plot(
-        arguments,
-        [math.sin(a) * math.sin(a / 20.0) for a in arguments]
+        arguments,  # передаем значения для построения графика
+        [math.sin(a) * math.sin(a / 20.0) for a in arguments]  # сама функция графика
     )
-    mpp.show()
+    mpp.show()  # показ графика
