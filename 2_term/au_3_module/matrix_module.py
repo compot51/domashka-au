6683957b008bd45f0210c7269e5a0412601b537b
Yakov <72313050+compot51@users.noreply.github.com>
2023-03-07 06:37:15 +0300

    Create matrix_module.py

---
+++ 2_term/au_3_module/matrix_module.py
@@ -0,0 +1,99 @@
+class Matrix:
+
+	def __init__(self, length, width, fill = 0):
+		self.length = length
+		self.width = width
+		self.matrix = [[fill] * self.width for i in range(self.length)]
+
+	def __getitem__(self, key):
+		if isinstance(key, tuple):
+			i = key[0]
+			j = key[1]
+			return self.matrix[i][j]
+
+
+	def __setitem__(self, key, value):
+		if isinstance(key, tuple):
+			i = key[0]
+			j = key[1]
+			self.matrix[i][j] = value
+
+	def __str__(self):
+		res = ""
+		for i in range(len(self.matrix)):
+			res += str(self.matrix[i]) + "\n"
+		return res
+	
+	def set_in_a_row_length(self):
+		res = Matrix(self.length, self.width)
+		num = 1
+		for i in range(self.length):
+			for j in range(self.width):
+				res.matrix[i][j] = num
+				num += 1
+		return res
+
+	def set_in_a_row_width(self):
+		res = Matrix(self.length, self.width)
+		num = 1
+		for i in range(self.width):
+			for j in range(self.length):
+				res.matrix[j][i] = num
+				num += 1
+		return res
+
+	def set_id_matrix(self):
+		res = Matrix(self.length, self.width)
+		for i in range(self.length):
+			for j in range(self.width):
+				res.matrix[i][j] = 1 if i == j else 0
+		return res
+
+	def __add__(self, other):
+		res = Matrix(self.length, self.width)
+		if isinstance(other, (int, float)):
+			for i in range(self.length):
+				for j in range(self.width):
+					res.matrix[i][j] = self.matrix[i][j] + other
+		elif isinstance(other, Matrix):
+			for i in range(self.length):
+				for j in range(self.width):
+					res.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
+			
+		return res
+
+	def __sub__(self, other):
+
+		res = Matrix(self.length, self.width)
+
+		if isinstance(other, (int, float)):
+			for i in range(self.length):
+				for j in range(self.width):
+					res.matrix[i][j] = self.matrix[i][j] - other
+		
+		elif isinstance(other, Matrix):
+			for i in range(self.length):
+				for j in range(self.width):
+					res.matrix[i][j] = self.matrix[i][j] - other.matrix[i][j]
+		return res
+	
+	def __mul__(self, other):
+		if isinstance(other, (int, float)):
+			res = Matrix(self.length, self.width)
+			for i in range(self.length):
+				for j in range(self.width):
+					res.matrix[i][j] = self.matrix[i][j] * other
+		elif isinstance(other, Matrix):
+			res = Matrix(self.length, other.width)
+			for i in range(self.length):
+				for j in range(other.width):
+					for k in range(other.length):
+						res.matrix[i][j] += self.matrix[i][k] * other.matrix[k][j]
+		return res				
+
+	def transpose(self):
+		res = Matrix(self.width, self.length)
+		for i in range(self.width):
+			for j in range(self.length):
+				res.matrix[i][j] = self.matrix[j][i]
+		return res
