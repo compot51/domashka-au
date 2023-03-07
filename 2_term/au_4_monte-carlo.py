e4974e48e2352db8c637f811058a99995f26f879
Yakov <72313050+compot51@users.noreply.github.com>
2023-03-07 17:57:26 +0300

    Update au_4_monte-carlo.py

--- 2_term/au_4_monte-carlo.py
+++ 2_term/au_4_monte-carlo.py
@@ -1,19 +1,32 @@
-# Вычисление числа π
 import random
 from numpy import arange
+from numba import jit
 
-n = 100000
-scal_val = 1 / n
+@jit(nopython="True")
+def solve():
+	n = 1000000
+	scal_val = 1 / n
+	cnt_in = 0
+	cnt_out = 0
 
-cnt_in = 0  # Счётчик попаданий точек в круг
-cnt_out = 0  # Ан-но непопаданий
+	for x in arange(0, 1, scal_val):
+		y = random.random()
+		if x ** 2 + y ** 2 <= 1:
+			cnt_in += 1
+		else:
+			cnt_out += 1
 
-for x in arange(0, 1, scal_val):
-	y = random.random()
-	if x ** 2 + y ** 2 <= 1:
-		cnt_in += 1
-	else:
-		cnt_out += 1
+	rel = cnt_in / cnt_out
+	return 4 * rel / (1 + rel)
 
-rel = cnt_in / cnt_out
-print(4 * rel / (1 + rel))  # Выводится из ур-ия соотношений площадей попадающих и непопадающих в круг точек
+
+a = tmp = 0
+t = 100
+
+while t:
+	# tmp = solve()
+	a += solve()
+	# print(a - tmp)
+	t -= 1
+
+print(a / 100)
